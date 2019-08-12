e78382c3e1fde59a062a66ed320033f6eb1d78a8
Alexander Root <aroot222@mit.edu>
2019-08-12 11:02:26 -0700

    better manual schedule + update im estimates

--- apps/max_pool_layer/max_pool_layer_generator.cpp
+++ apps/max_pool_layer/max_pool_layer_generator.cpp
@@ -35,14 +35,14 @@ public:
         if (auto_schedule) {
 
             // set some arbitray bounds estimates for the input
-            input.dim(0).set_estimate(0, 1024);
-            input.dim(1).set_estimate(0, 1024);
+            input.dim(0).set_estimate(0, 5048);
+            input.dim(1).set_estimate(0, 5048);
             input.dim(2).set_estimate(0, 3);
-            input.dim(3).set_estimate(0, 100);
+            input.dim(3).set_estimate(0, 10);
 
             // some common max pooling hyperparameters
-            stride.set_estimate(4);
-            extent.set_estimate(4);
+            stride.set_estimate(2);
+            extent.set_estimate(2);
 
             output.dim(0).set_estimate(0, 512); // w2 = (w1 - extent) / stride + 1
             output.dim(1).set_estimate(0, 512); // h2 = (h1 - extent) / stride + 1
@@ -50,9 +50,7 @@ public:
             output.dim(3).set_estimate(0, 100);
         }
         else {
-            Var x_outer, x_inner, y_outer, y_inner;
-            output.tile(x, y, x_outer, y_outer, x_inner, y_inner, 4, 4).parallel(n); //.vectorize(x, 8);
-            // output.reorder(n, z).parallel(z).vectorize(x, 8);
+            output.parallel(n).vectorize(x, 16); // simple but effective
         }
    }
 };
